e2a754c213e5abe0e604c63ee2d46a45599748df
Emmeline <Emmeline.Lan@gmail.com>
2017-02-20 18:02:36 -0800

    created projects interactive gallery component

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 
+//components
+import ProjectsGallery from './ProjectsGallery.js'
+
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <div>hi</div>
+    return (
+      <div className='main-container'>
+        <h1>Project Overview</h1>
+        <ProjectsGallery />
+      </div>
+    )
   }
 }
