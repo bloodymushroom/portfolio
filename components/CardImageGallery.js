2cad5b75c0757a475cfb91cd754bd3b8b117f2a8
Emmeline <Emmeline.Lan@gmail.com>
2017-02-23 04:12:28 -0800

    started gallery for other skills

--- components/CardImageGallery.js
+++ components/CardImageGallery.js
@@ -1,1 +1,27 @@
-import React 
+import React, {Component} from 'react'
+
+export default class CardImageGallery extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  generateCard(image, label) {
+    return (
+      <div className='card-container'>
+        Gallery Coming Soon!
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className='gallery-container'>
+        <div className='arrow left-arrow'></div>
+        <div className='gallery'>
+          {this.generateCard()}
+        </div>
+        <div className='arrow right-arrow'></div>
+      </div>
+    )
+  }
+}
